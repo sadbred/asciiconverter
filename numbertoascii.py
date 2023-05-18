f7b42f4e130a66572195a9fef5befa7f1e2f37bd
sadbred <117604119+sadbred@users.noreply.github.com>
2023-05-18 15:56:44 -0500

    Add files via upload

---
+++ numbertoascii.py
@@ -0,0 +1,22 @@
+import os
+
+def convert_numbers_to_ascii(numbers):
+    ascii_chars = []
+    number_list = numbers.split()
+
+    for num in number_list:
+        try:
+            ascii_char = chr(int(num))
+            ascii_chars.append(ascii_char)
+        except ValueError:
+            pass
+
+    ascii_text = ''.join(ascii_chars)
+    return ascii_text
+
+numbers = input("Enter the numbers separated by spaces: ")
+result = convert_numbers_to_ascii(numbers)
+print("ASCII text:", result)
+
+input("Press Enter to clear the screen...")
+os.system('cls' if os.name == 'nt' else 'clear')
